04cadac074b6e50929d3595fe8fc849645f12c11
gtovar1985 <gtovar@incaztech.com>
2015-01-19 00:17:46 -0600

    update user try1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-@admin = Admin.new(:email => ENV['USER_EMAIL'], :password => ENV['USER_PASSWORD'], :password_confirmation =>     ENV['USER_PASSWORD'])
+@admin = Admin.new(:email => ENV['USER_EMAIL'], :password => ENV['USER_PASSWORD'], :password_confirmation => ENV['USER_PASSWORD'])
 if @admin.save
   puts "Admin was created"
 else
