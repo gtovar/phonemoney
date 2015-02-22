b7727cda72aa861085ee3b036bf9f2a93bd05503
gtovar1985 <gtovar@incaztech.com>
2015-02-21 22:53:15 -0600

    New Sign In New Layout is READY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
 
+  protected
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "sign_in_layout"
+    else
+      "application"
+    end
+  end
 end
